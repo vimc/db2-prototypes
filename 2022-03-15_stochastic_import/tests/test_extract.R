f7290ac9f78d344a1db4421238e09ce3abe8c70f
Robert Ashton <r.ashton@imperial.ac.uk>
2022-03-15 17:07:36 +0000

    Fix test

--- 2022-03-15_stochastic_import/tests/test_extract.R
+++ 2022-03-15_stochastic_import/tests/test_extract.R
@@ -7,7 +7,7 @@ testthat::test_that("extracted data is as expected", {
                   c("run_ids", "scenarios", "cases", "data"))
   expect_equal(extracted_data$run_ids, 1:200)
   expect_length(extracted_data$scenarios, 5)
-  expect_length(extracted_data$cases)
+  expect_equal(nrow(extracted_data$cases), 1000)
   expect_length(extracted_data$data, 1000)
   for (df in extracted_data$data) {
     expect_true(nrow(df) > 0)
