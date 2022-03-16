5703d53fcd813740605539ff1e53fb9fbbd7bb56
Robert Ashton <r.ashton@imperial.ac.uk>
2022-03-16 10:39:25 +0000

    Update import to create all required files

--- 2022-03-16_add_stochastic_aggregate_tables/tests/test_load.R
+++ 2022-03-16_add_stochastic_aggregate_tables/tests/test_load.R
@@ -1,5 +1,7 @@
 context("load")
 
 test_that("new tables added", {
-  expect_equal(setdiff(after, before), c("stochastic_1"))
+  expect_equal(setdiff(after, before),
+               c("stochastic_file", "stochastic_1", "stochastic_2",
+                 "stochastic_3", "stochastic_4"))
 })
