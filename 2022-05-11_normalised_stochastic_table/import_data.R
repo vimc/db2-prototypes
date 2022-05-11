2b2df3df981cb06ec5fba49108bcb65c7c0fbb55
Robert Ashton <r.ashton@imperial.ac.uk>
2022-05-11 16:12:14 +0100

    Create a normalised table for testing

---
+++ 2022-05-11_normalised_stochastic_table/import_data.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+
+import <- function() {
+  con <- dettl:::db_connect("local", ".")
+  DBI::dbBegin(con)
+  DBI::dbExecute(con, "CREATE TABLE stochastic_all_normalised (
+  stochastic_id integer
+  run_id integer
+  year integer
+  cohort integer
+  country text
+  metric text
+  value double
+)")
+
+  data <- dplyr::tbl(con, "stochastic_all") %>%
+    tidyr::pivot_longer("cases_yf-no-vaccination":"dalys_measles-mcv2-ia2030_target",
+                        names_to = "metric", values_drop_na = TRUE)
+  DBI::dbAppendTable(con, "stochastic_all_normalised", data)
+  DBI::dbCommit(con)
+}
+
+start <- Sys.time()
+import()
+end <- Sys.time()
+time <- end - start
+msg <- paste0("Import ", time, " ", attr(time, "units"))
+message(msg)
+output_file <- "timings.txt"
+if (!file.exists(output_file)) {
+  file.create(output_file)
+}
+write(msg, file = output_file, append = TRUE)
